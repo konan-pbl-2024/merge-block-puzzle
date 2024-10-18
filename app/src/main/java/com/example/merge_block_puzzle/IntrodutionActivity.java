f419d75e3b8abcfd920208dd4f3c85bd6e594bcb
ryomii <ryomii1020@icloud.com>
2024-10-18 17:35:44 +0900

    introを編集

--- app/src/main/java/com/example/merge_block_puzzle/IntrodutionActivity.java
+++ app/src/main/java/com/example/merge_block_puzzle/IntrodutionActivity.java
@@ -2,13 +2,27 @@ package com.example.merge_block_puzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class IntrodutionActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introdution);
+        Button okButton=(Button)findViewById(R.id.button);
+        okButton.setOnClickListener(new View.OnClickListener()){
+            public void onClick(View ){
+
+            }
+        }
     }
+
+
+
 }

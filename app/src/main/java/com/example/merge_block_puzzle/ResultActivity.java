8ebfa3c386cee2fd1897db063c14b0dd180eb128
Hirootsugu <hirootugu@icloud.com>
2024-10-18 17:35:18 +0900

    10/18やりました

--- app/src/main/java/com/example/merge_block_puzzle/ResultActivity.java
+++ app/src/main/java/com/example/merge_block_puzzle/ResultActivity.java
@@ -2,7 +2,10 @@ package com.example.merge_block_puzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Button returnbutton = (Button)findViewById(R.id.button1);
+        returnbutton.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v) {
+                    Intent intent = new Intent(ResultActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+        });
     }
 }

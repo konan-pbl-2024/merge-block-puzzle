00f28d4aefc9a61effca49d3215fec7fef0d207a
kp8 <natsuki111124@gmail.com>
2024-11-08 16:33:33 +0900

    パズルが動くまで作りました

--- app/src/main/java/com/example/merge_block_puzzle/PuzzleActivity.java
+++ app/src/main/java/com/example/merge_block_puzzle/PuzzleActivity.java
@@ -1,14 +1,30 @@
 package com.example.merge_block_puzzle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+//import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.gridlayout.widget.GridLayout;
 
 public class PuzzleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_puzzle);
+
+        GridLayout gridLayout = findViewById(R.id.puzzleArea);
+        com.example.merge_block_puzzle.PuzzleBoard puzzleBoard = new com.example.merge_block_puzzle.PuzzleBoard(this);
+        gridLayout.addView(puzzleBoard);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.puzzle), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }

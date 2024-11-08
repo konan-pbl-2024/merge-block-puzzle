32bbfc0d8ac8a3f37149ba1ff366fee62c33708d
Hirootsugu <hirootugu@icloud.com>
2024-11-08 16:53:41 +0900

    a

--- app/src/main/java/com/example/merge_block_puzzle/MainActivity.java
+++ app/src/main/java/com/example/merge_block_puzzle/MainActivity.java
@@ -14,6 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button p_button = (Button)findViewById(R.id.play_button2);
+        p_button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View i){
+                Intent intent = new Intent(MainActivity.this,PuzzleActivity.class);
+                startActivity(intent);
+            }
+        });
+
         Button s_button = (Button)findViewById(R.id.start_button);
         s_button.setOnClickListener(new View.OnClickListener(){
            public void onClick(View v){
